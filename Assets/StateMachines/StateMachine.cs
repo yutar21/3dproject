60155c3274ba23fea52b8e260872d1e5a66b527a
Yutar <100964305+yutar21@users.noreply.github.com>
2024-08-08 14:53:49 +0700

    setupproject

---
+++ Assets/StateMachines/StateMachine.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine
+{
+    protected IState currentState;
+    public void ChangeState(IState newState)
+    {
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+    public void HandleInput()
+    {
+        currentState.HandleInput();
+    }
+    public void Update()
+    {
+        currentState.Update();
+    }
+    public void PhysicsUpdate()
+    {
+        currentState.PhysicsUpdate();
+    }
+}
